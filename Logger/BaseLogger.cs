800a2723c88124e31505503057f84977f16a52e2
mhtran23 <mtran23@ewu.edu>
2023-01-22 20:09:47 -0800

    updating files
    created FileLogger, updated: LogFactory, BaseLogger, BasLoggerMixins

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -1,7 +1,9 @@
-﻿namespace Logger
+﻿using System;
+namespace Logger
 {
     public abstract class BaseLogger
     {
+        public string ClassName { get; set; }
         public abstract void Log(LogLevel logLevel, string message);
     }
 }
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,11 +1,16 @@
-﻿namespace Logger
+﻿using System;
+
+namespace Logger
 {
     public class LogFactory
     {
+        private string _FilePath;
         public BaseLogger CreateLogger(string className)
         {
-            
-            return null;
+           if(_FilePath== null) { return null; }
+           return new FileLogger(_FilePath) { ClassName = className };
         }
+
+        public void ConfigureFileLogger(string filePath) { _FilePath = filePath; }
     }
 }
