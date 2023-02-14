4e119aa56b3fa710cec51f68239649df48a4f301
mhtran23 <mtran23@ewu.edu>
2023-02-13 20:11:13 -0800

    GenericsHomework added

---
+++ GenericsHomework.Test/NodeTest.cs
@@ -0,0 +1,94 @@
+﻿using GenericsHomework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GenericsHomeworkTests;
+
+[TestClass]
+public class NodeTests
+{
+    [TestMethod]
+    public void NodeTypeString_Are_Equal()
+    {
+        Node<string> newNode = new("one");
+        Assert.AreEqual<string>("one", newNode.Value);
+
+        Node<string> newNode2 = new("two", newNode);
+        Assert.AreEqual<string>("two", newNode2.Value);
+    }
+
+    [TestMethod]
+    public void NodeIsLast_True()
+    {
+        Node<string> newNode = new("one");
+        Assert.IsTrue(newNode.Equals(newNode.Last));
+
+        Node<string> newNode2 = new("two", newNode);
+        Assert.IsTrue(newNode2.Equals(newNode2.Last));
+    }
+
+    [TestMethod]
+    public void NodeExists_True()
+    {
+        Node<string> newNode = new("23");
+        Assert.IsTrue(newNode.Exists("23"));
+    }
+
+    [TestMethod]
+    public void NodeExists_False()
+    {
+        Node<string> newNode = new("11");
+        Assert.IsFalse(newNode.Exists("12"));
+    }
+
+    [TestMethod]
+    public void NodeExists_Null_True()
+    {
+        string empty = null!;
+        Node<string> newNode = new("one");
+        newNode.Append("two");
+        newNode.Append(empty);
+        newNode.Append("three");
+
+        Assert.IsTrue(newNode.Exists(null!));
+    }
+
+    [TestMethod]
+    public void NodeAppend_DuplicateValue_throwsException()
+    {
+        _ = Assert.ThrowsException<ArgumentException>(() =>
+        {
+            Node<double> newNode = DoubleRadioStations();
+            newNode.Append(13);
+        });
+    }
+
+
+    [TestMethod]
+    public void NodeClear_RemovesAllNodesNotLast()
+    {
+        Node<double> newNode = DoubleRadioStations();
+        Assert.IsTrue(newNode.Exists(13));
+        Assert.IsTrue(newNode.Exists(88));
+        Assert.IsTrue(newNode.Exists(99));
+        Assert.IsTrue(newNode.Exists(100));
+        Assert.IsTrue(newNode.Exists(108));
+
+        newNode.Clear();
+        Assert.IsTrue(newNode.Exists(13));
+        Assert.IsFalse(newNode.Exists(88));
+        Assert.IsFalse(newNode.Exists(99));
+        Assert.IsFalse(newNode.Exists(100));
+        Assert.IsFalse(newNode.Exists(108));
+    }
+
+    public static Node<double> DoubleRadioStations()
+    {
+        Node<double> newNode = new(13);
+        newNode.Append(88);
+        newNode.Append(99);
+        newNode.Append(100);
+        newNode.Append(108);
+        return newNode;
+    }
+}
---
+++ GenericsHomework/Node.cs
@@ -0,0 +1,76 @@
+﻿namespace GenericsHomework
+{
+    public class Node<TValues> where TValues : notnull
+    {
+        public TValues Value { get; set; }
+        public Node<TValues> Next { get; private set; }
+        public Node<TValues> Last;
+
+        public Node(TValues value)
+        {
+            Value = value;
+            Next = this;
+            Last = this;
+        }
+
+        public Node(TValues value, Node<TValues> node)
+        {
+            Value = value;
+            Next = node;
+            Last = this;
+        }
+
+        public void Append(TValues value)
+        {
+            bool keyValueExists = Exists(value);
+            if (keyValueExists == true)
+            {
+                throw new ArgumentException("Cannot add duplicate value");
+            }
+            Node<TValues> lastNode = GetLast();
+            lastNode.Next = new Node<TValues>(value, Last);
+        }
+
+        public Node<TValues> GetLast()
+        {
+            Node<TValues> currentNode = Last;
+            while (currentNode.Next != Last)
+            {
+                currentNode = currentNode.Next;
+            }
+            return currentNode;
+        }
+        public void Clear()
+        {
+
+            Last.Next = this;
+        }
+        public Boolean Exists(TValues key)
+        {
+            Node<TValues> currentNode = Last;
+            do
+            {
+                if (key is null)
+                {
+                    if (currentNode.Value is null)
+                        return true;
+                }
+                else if (key.Equals(currentNode.Value))
+                    return true;
+                currentNode = currentNode.Next;
+            } while (currentNode != Last);
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string? stringReturn = null;
+            if (Value is not null)
+                stringReturn = Value.ToString();
+            if (stringReturn is null)
+                return "";
+            return stringReturn;
+        }
+    }
+}
