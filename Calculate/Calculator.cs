0111d03d00e7ef434c241a2fbabcb22b0290022d
mhtran23 <mtran23@ewu.edu>
2023-02-27 01:09:18 -0800

    updated Calculate class

---
+++ Calculate/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculate;
+
+public class Calculator
+{
+    public static Func<int, int, int> Add => (x, y) => x + y;
+    public static Func<int, int, int> Subtract => (x, y) => x - y;
+    public static Func<int, int, int> Multiply => (x, y) => x * y;
+    public static Func<int, int, int> Divide => (x, y) =>
+    {
+        if (y == 0)
+        {
+            throw new DivideByZeroException("Can not divide by zero");
+        }
+        else
+        { return x / y; }
+    };
+
+    public static IReadOnlyDictionary<char, Func<int, int, int>> MathematicalOperations { get; } = new Dictionary<char, Func<int, int, int>>()
+    {
+        {'+', Add },
+        {'-', Subtract },
+        {'*', Multiply },
+        {'/', Divide },
+    };    
+    
+    public static bool TryCalculate(string calculation, out int result)
+    {
+        result = 0;
+        int operand1, operand2;
+        char operator1;
+        
+        string[] input = calculation.Split(' ');
+        
+        if (input.Length != 3) 
+        {
+            return false;
+        }
+        else if(!int.TryParse(input[0], out operand1 )|| !char.TryParse(input[1], out operator1) 
+            || !int.TryParse(input[2], out operand2))
+        {  
+            return false; 
+        }
+        else if(!Calculator.MathematicalOperations.TryGetValue(operator1,out var operation))
+        {
+            return false;
+        }
+        else
+        {
+            result = operation(operand1, operand2);
+            return true;
+        }
+
+    }
+}
---
+++ Calculate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate
+{
+    public class Program
+    {
+        public Action<string> WriteLine { get; init; } = Console.WriteLine;
+        public Func<string?> ReadLine { get; init;} = Console.ReadLine;
+
+        public Program() { }
+        static void Main(string[] args) 
+        {
+            Program progarm = new();
+            Calculator cal = new();
+            int result;
+            bool correctInput = true;
+
+            while (true)
+            {
+                progarm.WriteLine("Enter an equation with spaces inbetween the operands and operator(ex: '5 + 3'): ");
+                string? userInput = progarm.ReadLine.Invoke();
+                correctInput = cal.TryCalculate(userInput, out result);
+                if (correctInput)
+                {
+                  progarm.WriteLine(result.ToString());
+                }
+                else
+                {
+                    progarm.WriteLine.Invoke("Invalid input");
+                }
+            }
+        
+        }
+
+    }
+}
